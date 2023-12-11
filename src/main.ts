cd975e776aeb3d362ddca974948f0db7e5d057d6
Olavur Ellefsen <oe@tokni.com>
2023-12-11 14:50:07 +0000

    feat: Set event-time to validTime

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 // This is the main entrypoint to the transformer, it should not be
 // necessary to modify this file for a basic transformer.
 // -------------------------------------------------------------------
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import health from "@functions/health.entrypoint";
 import transform from "@functions/transform.entrypoint";
 import start from "@functions/start.entrypoint";
@@ -17,8 +17,8 @@ app.get("/health", async (req: Request, res: Response): Promise<Response> => {
 
 app.post("/transform", async (req: Request, res: Response): Promise<Response> => {
   const { eventId, validTime, payload } = req.body;
-
-  return res.send(await transform({
+  
+  return res.set("x-flowcore-event-time", validTime).send(await transform({
     eventId,
     validTime,
     payload,
